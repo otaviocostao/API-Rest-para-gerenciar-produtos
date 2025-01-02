d5de0cee23ee768e86926abfe1ee57bd4d5a4bfa
Otavio <dev.otaviocostao@gmail.com>
2025-01-02 19:53:27 -0300

    Criação de endpoints do controller

---
+++ src/main/java/dio/cadastro_produtos/controller/ProdutoController.java
@@ -0,0 +1,36 @@
+package dio.cadastro_produtos.controller;
+
+import dio.cadastro_produtos.entity.Produto;
+import dio.cadastro_produtos.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/produto")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<Produto> produtoSave(@RequestBody Produto produto){
+        produto = produtoService.saveProduto(produto);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Produto> produtoFind(@PathVariable Long id){
+        Produto produto = produtoService.findById(id);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @GetMapping(value = "/busca-todos")
+    public ResponseEntity<List<Produto>> produtoFind(){
+        List<Produto> produtos = produtoService.findAll();
+        return ResponseEntity.ok().body(produtos);
+    }
+}
